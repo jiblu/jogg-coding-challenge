8849a2ce71e4d4bbacf328e757b7345ea22c13ac
ji <jkim0107@gmail.com>
2020-05-08 11:47:35 -0700

    content conditionally rendering view

--- components/content.js
+++ components/content.js
@@ -1,12 +1,16 @@
 import GridView from './gridView'
 import ListView from './listView'
 import { Page } from '../styles/component'
-// conditionally render gridview vs listview to content
 
-const Content = props => (
-  <Page>
-    Content here {props.isGrid.toString()}
-  </Page>
-)
+const Content = props => {
+  let view = props.isGrid ?
+    <GridView items={props.items} /> :
+    <ListView items={props.items} />
+  return (
+    <Page>
+      {view}
+    </Page>
+  )
+}
 
 export default Content
