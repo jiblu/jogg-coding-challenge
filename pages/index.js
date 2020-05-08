fe97d6fb0a86393f4fd6ae573687c0f59873d556
ji <jkim0107@gmail.com>
2020-05-08 10:59:49 -0700

    index to stateful

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,38 @@
 import Content from '../components/content'
 import Nav from '../components/nav'
 
-export default function Index() {
-  const getData = async () => {
-    const data = await fetch("/api/cards").then((res) => res.json());
-    console.log(data);
+export default class Index extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: []
+    }
+  }
+
+  getData = async () => {
+    const data = await fetch("/api/cards")
+      .then((res) => res.json())
+      .then(json => {this.setState({items: json})})
+      .catch(err => {console.log(`error: ${err}`)})
   };
 
-  getData();
+  componentDidMount() {
+    this.getData()
+  }
 
-  return (
-    <div>
-      <Nav />
-      <Content />
-      <style jsx>{`
-        :global(body) {
-          margin: 0;
-          font-family: -apple-system,BlinkMacSystemFont,Avenir Next,Avenir,Helvetica,sans-serif;
-        }
-      `}
-      </style>
-    </div>
-  )
+  render() {
+    return (
+      <div>
+        <Nav />
+        <Content items={this.state.items} />
+        <style jsx>{`
+          :global(body) {
+            margin: 0;
+            font-family: -apple-system,BlinkMacSystemFont,Avenir Next,Avenir,Helvetica,sans-serif;
+          }
+        `}
+        </style>
+      </div>
+    )
+  }
 }
