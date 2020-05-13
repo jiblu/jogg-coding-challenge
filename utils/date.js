3591a2db5d59a6a914c0e18ed80653f2d0c3fc53
ji <jkim0107@gmail.com>
2020-05-12 19:08:16 -0700

    fixed date formatting for full month

--- utils/date.js
+++ utils/date.js
@@ -1,10 +1,10 @@
 // convert to MMM-DD-YYYY format
 export const getFormattedDate = (d) => {
   var date = new Date(d)
-  var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
+  var months = ['January','February','March','April','May','June','July','August','September','October','November','December']
   var dd = date.getDate();
-  var mmm = months[date.getMonth()]; 
+  var mmmm = months[date.getMonth()]; 
   var yyyy = date.getFullYear();
 
-  return `${mmm} ${dd} ${yyyy}`
+  return `${mmmm} ${dd}, ${yyyy}`
 }
