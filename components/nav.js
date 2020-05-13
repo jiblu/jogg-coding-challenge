f9017b924fb1d8537a43317d71ee08d24ac1aaae
ji <jkim0107@gmail.com>
2020-05-12 19:29:40 -0700

    fixed dynamic svg color toggle

--- components/nav.js
+++ components/nav.js
@@ -1,30 +1,34 @@
 import styled from 'styled-components'
-import { Metrics } from '../styles'
+import { Metrics, Colors } from '../styles'
 import { NavTitleStyle, NavStyle, IconsStyle, IconStyle, SpacerStyle } from '../styles/componentStyles'
 import GridIcon from '../svgs/grid-view.svg'
+import GridIconActive from '../svgs/grid-view-active.svg'
 import ListIcon from '../svgs/list-view.svg'
+import ListIconActive from '../svgs/list-view-active.svg'
 
-const Nav = props => (
-  <NavStyle>
-      <NavTitleStyle>My Joggs</NavTitleStyle>
-      <SpacerStyle />
-      <IconsStyle>
-        <IconStyle>
-          <GridIcon
-            width={Metrics.sizes.xsmall}
-            height={Metrics.sizes.xsmall}
-            onClick={() => props.changeView(true)}
-          />
-        </IconStyle>
-        <IconStyle>
-          <ListIcon
-            width={Metrics.sizes.xsmall}
-            height={Metrics.sizes.xsmall}
-            onClick={() => props.changeView(false)}
-          />
-        </IconStyle>
-      </IconsStyle>
-  </NavStyle>
-)
 
+const Nav = props => {
+let gridIcon = props.isGrid ? 
+  <GridIconActive onClick={() => props.changeView(true)} width={Metrics.sizes.xsmall} height={Metrics.sizes.xsmall}/> :
+  <GridIcon onClick={() => props.changeView(true)} width={Metrics.sizes.xsmall} height={Metrics.sizes.xsmall}/>
+
+let listIcon = !props.isGrid ? 
+  <ListIconActive onClick={() => props.changeView(false)} width={Metrics.sizes.xsmall} height={Metrics.sizes.xsmall}/> :
+  <ListIcon onClick={() => props.changeView(false)} width={Metrics.sizes.xsmall} height={Metrics.sizes.xsmall}/>
+
+  return (
+    <NavStyle>
+        <NavTitleStyle>My Joggs</NavTitleStyle>
+        <SpacerStyle />
+        <IconsStyle>
+          <IconStyle>
+            {gridIcon}
+          </IconStyle>
+          <IconStyle>
+            {listIcon}
+          </IconStyle>
+        </IconsStyle>
+    </NavStyle>
+  )
+}
 export default Nav
